f31fed3a654c9d2e6c9d8b366571f03ada53d561
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2023-12-08 21:30:28 +0100

    Make nix work

--- flake.nix
+++ flake.nix
@@ -3,19 +3,29 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix/monthly";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      });
+  outputs = { self, nixpkgs, utils, naersk, fenix }:
+    utils.lib.eachDefaultSystem (system: {
+      packages.default = let
+        pkgs = nixpkgs.legacyPackages.${system};
+        target = "x86_64-unknown-linux-gnu";
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.cargo
+            minimal.rustc
+            targets.${target}.latest.rust-std
+          ];
+
+      in (naersk.lib.${system}.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      }).buildPackage {
+        doCheck = true;
+        src = ./.;
+        CARGO_BUILD_TARGET = target;
+      };
+    });
+
 }
